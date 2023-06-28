ac9a62f3686ac5f9e16a7264a0c2a8ed55f8de66
bibhashanand <bibhashanand@outlook.com>
2023-06-28 10:51:21 +0530

    Day3

---
+++ 3Day2-First-JS/2write-ext.js
@@ -0,0 +1,8 @@
+document.write("<h1>Hello World this is external JS</h1>")
+
+var a = 50;
+var b = 20;
+
+sum = a+b;
+
+document.write("<h2> The sum of "+a +" and "+b +" is "+sum +"</h2>")
---
+++ 3Day2-First-JS/3calc.js
@@ -0,0 +1,31 @@
+function Addition() {
+    // Get the values from the form
+    var a = parseFloat(document.getElementById('a').value);
+    var b = parseFloat(document.getElementById('b').value);
+    var result = a + b;
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+  }
+
+function Subtraction() {
+    // Get the values from the form
+    var a = parseFloat(document.getElementById('a').value);
+    var b = parseFloat(document.getElementById('b').value);
+    var result = a - b;
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+  }
+
+function Multiplication() {
+    // Get the values from the form
+    var a = parseFloat(document.getElementById('a').value);
+    var b = parseFloat(document.getElementById('b').value);
+    var result = a * b;
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+  }
+
+function Division() {
+    // Get the values from the form
+    var a = parseFloat(document.getElementById('a').value);
+    var b = parseFloat(document.getElementById('b').value);
+    var result = a / b;
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+  }
