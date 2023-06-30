c6d7dff9685efd0dabdf67813ce2c95a6e6ec078
bibhashanand <bibhashanand@outlook.com>
2023-06-30 15:11:18 +0530

    with Nav Bar

--- 4Day3-React/vite-react-app1/src/App.tsx
+++ 4Day3-React/vite-react-app1/src/App.tsx
@@ -1,7 +1,8 @@
 import ListGroup from "./component/ListGroup";
+import Navbar from "./component/navbar";
 
 function App()
 {
-  return <div><ListGroup/></div>;
+  return <div><Navbar/><ListGroup/></div>;
 }
 export default App;
