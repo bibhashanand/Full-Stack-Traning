0f9edd1a081abcc25e8fa0939d536286fa1d2475
bibhashanand <bibhashanand@outlook.com>
2023-06-30 12:10:37 +0530

    update

--- 4Day3-React/vite-react-app/src/message.tsx
+++ 4Day3-React/vite-react-app/src/message.tsx
@@ -1,5 +1,8 @@
 function Message()
-{
+{   const name='Bibhash';
+    if(name)
+    return <h1>Hello {name} </h1>;
+    else
     return <h2>Hello World! it's React, build using Vite</h2>;
 }
 export default Message;
