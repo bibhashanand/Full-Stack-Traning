0ae5100ca51bd3050f56932b59f78e19651170b5
bibhashanand <bibhashanand@outlook.com>
2023-07-03 10:47:15 +0530

    another way to use condition

--- 4Day3-React/vite-react-app1/src/component/ListGroup.tsx
+++ 4Day3-React/vite-react-app1/src/component/ListGroup.tsx
@@ -13,19 +13,28 @@ function listgroup()
         'Bhuvneshwar'
     ];
 
-    //items = [];
+    items = [];
 
-    if (items.length === 0) // === is strict comparasion which compare values as well as data type
-        return <p>No item found</p>;
+    // if (items.length === 0) // === is strict comparasion which compare values as well as data type
+    //     return (
+    //     <>
+    //     <h5>List</h5>
+    //     <p>No item found</p>
+    //     </>
+    //     );
         
 
-return (
-<ol className="list-group list-group-numbered">
-    {items.map(item=> (
-    <li className="list-group-item" key={item}>{item}</li>
-    ))}
-</ol>
-);
+    return (
+        <>
+        <h5>List</h5>
+        { items.length == 0 ? <p>No items found</p> : null}
+        <ol className="list-group list-group-numbered">
+            {items.map(item=> (
+                 <li className="list-group-item" key={item}>{item}</li>
+                ))}
+        </ol>
+        </>
+        );
 }
 
 export default listgroup;
